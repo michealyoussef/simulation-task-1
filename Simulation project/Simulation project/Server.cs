f2c6c1e99db4083d257ddae6508db77bc6a7ef54
michealyoussef <michealysha@yahoo.com>
2015-10-12 17:09:35 +0200

    making server class

--- Simulation project/Simulation project/Server.cs
+++ Simulation project/Simulation project/Server.cs
@@ -13,19 +13,41 @@ namespace Simulation_project
         public int[] serviceTime;
         public float[] cumulativeprobility;
 
-        public Server(int per, float[] p, int[] ST, float[] CP)
+        public Server(int[] IT, int per, float[] p, int[] ST, float[] CP)
         {
+
             this.periorty = per;
             this.probility = p;
             this.serviceTime = ST;
         }
         public void calcule_cumulativeprobility()
         {
-            for(int w=0;w<probility.Length;w++)
+            float sum = 0;
+            for (int w = 0; w < probility.Length; w++)
             {
-
+                sum += probility[w];
+                cumulativeprobility[w] = sum;
             }
         }
+        public float randfunction()
+        {
 
+            Random r = new Random();
+            float w = r.Next(0, 100);
+            if (w > 0 && w < cumulativeprobility[0] * 100)
+            {
+                return this.cumulativeprobility[0] * 100;
+            }
+            for (int a = 1; a < cumulativeprobility.Length - 1; a++)
+            {
+                if (cumulativeprobility[a - 1] < w && w < cumulativeprobility[a] * 100)
+                {
+                    break;
+                    return serviceTime[a];
+
+                }
+            }
+            return 0;
+        }
     }
 }
