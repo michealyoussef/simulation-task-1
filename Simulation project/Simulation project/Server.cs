d92bca227774ce8350801f295ff7695f8d36b8bd
michealyoussef <michealysha@yahoo.com>
2015-10-12 16:31:12 +0200

    make

---
+++ Simulation project/Simulation project/Server.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulation_project
+{
+    class Server
+    {
+        public int periorty;
+        public float[] probility;
+        public int[] serviceTime;
+        public float[] cumulativeprobility;
+
+        Server(int per, float[] p, int[] ST, float[] CP)
+        {
+            this.periorty = per;
+            this.probility = p;
+            this.serviceTime = ST;
+        }
+    }
+}
